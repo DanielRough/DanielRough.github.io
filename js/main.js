3945b65b29b771a94ed7f733fd22f8cb8c2c9d5a
DanielRough <djr53@st-andrews.ac.uk>
2022-03-09 19:37:36 +0000

    adjusting js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest();
 
+console.log("HELLO!");
 // Open a new connection, using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
 
