d7f68deaa35591ab5dc52372d83f412de6817693
Daniel John Rough <danieljohnrough@Daniels-MBP-2.lan>
2022-03-11 03:27:25 +0000

    Updated js

--- js/main.js
+++ js/main.js
@@ -9,6 +9,14 @@ request.onload = function () {
 	 var data = JSON.parse(this.response);
 	 console.log("Last value: " + data.last_value);
 	 console.log("Updated at: " + data.updated_at);
+	 var date = new Date(data.updated_at);
+	 console.log(date);
+	 if(data.last_value < 100){
+		 console.log("DARK");
+	 }
+	 else{
+		 console.log("LIGHT");
+	 }
 }
 
 // Send request
